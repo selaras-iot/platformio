73b5693d1e588b4c04576e0597421862d13a36c8
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-02 17:05:23 +0700

    feat: Add MQTT functionality and WiFi connection to main application

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,77 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <config.h>
 
-// put function declarations here:
-int myFunction(int, int);
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.publish("rdaOutTopic", "hello world");
+      client.subscribe("rdaInTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  // connect to wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("\nWiFi connected!");
+  Serial.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+
+  Serial.println("Connecting to MQTT broker...");
+  client.setServer(MQTT_BROKER, 1883);
+  client.setCallback(callback);
 }
+
+int value = 0;
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (!client.connected()) reconnect();
+  client.loop();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("rdaOutTopic", msg);
+  }
 }
