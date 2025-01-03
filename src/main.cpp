475f05d0e042b45d0fbd819821c8f8716bc59a73
Rizal Anggoro <gnoogler4@gmail.com>
2025-01-03 18:48:33 +0700

    feat: Add ESPAsyncWebServer and WebSerial for improved web interface and debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,75 @@
-#include <Arduino.h>
+#if defined(ESP8266)
 #include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+
+#include <Arduino.h>
+#include <ESPAsyncWebServer.h>
 #include <PubSubClient.h>
 #include <WS2812FX.h>
+#include <WebSerial.h>
 #include <config.h>
 
+AsyncWebServer server(80);
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 WS2812FX ws2812fx = WS2812FX(44, 5, NEO_GRB + NEO_KHZ800);
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  // Serial.print("Message arrived [");
-  // Serial.print(topic);
-  // Serial.print("] ");
-  // for (int i = 0; i < length; i++) {
-  //   Serial.print((char)payload[i]);
+  WebSerial.print("Message arrived [");
+  WebSerial.print(topic);
+  WebSerial.print("] ");
+  for (int i = 0; i < length; i++) {
+    WebSerial.print((char)payload[i]);
+  }
+  WebSerial.println();
+  // if (strcmp(topic,
+  //            ("com.rizalanggoro.selaras/" + String(DEVICE_UUID) + "/mode")
+  //                .c_str()) == 0) {
+  //   int mode = atoi((char*)payload);
+  //   ws2812fx.setMode(mode);
+  //   WebSerial.print("mode: ");
+  //   WebSerial.println(mode);
+  // } else if (strcmp(topic, ("com.rizalanggoro.selaras/" + String(DEVICE_UUID)
+  // +
+  //                           "/brightness")
+  //                              .c_str()) == 0) {
+  //   int brightness = atoi((char*)payload);
+  //   ws2812fx.setBrightness(brightness);
+  //   WebSerial.print("brightness: ");
+  //   WebSerial.println(brightness);
   // }
-  // Serial.println();
-  if (strcmp(topic,
-             ("com.rizalanggoro.selaras/" + String(DEVICE_UUID) + "/mode")
-                 .c_str()) == 0) {
-    int mode = atoi((char*)payload);
-    ws2812fx.setMode(mode);
-    Serial.print("mode: ");
-    Serial.println(mode);
-  } else if (strcmp(topic, ("com.rizalanggoro.selaras/" + String(DEVICE_UUID) +
-                            "/brightness")
-                               .c_str()) == 0) {
-    int brightness = atoi((char*)payload);
-    ws2812fx.setBrightness(brightness);
-    Serial.print("brightness: ");
-    Serial.println(brightness);
-  }
 }
 
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    Serial.println(clientId);
+boolean reconnect() {
+  WebSerial.print("Attempting MQTT connection...");
+  String clientId = "ESP8266Client-";
+  clientId += String(random(0xffff), HEX);
+  WebSerial.println(clientId);
 
-    if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // client.publish("rdaOutTopic", "hello world");
-      client.subscribe(
-          ("com.rizalanggoro.selaras/" + String(DEVICE_UUID) + "/mode")
-              .c_str());
-      client.subscribe(
-          ("com.rizalanggoro.selaras/" + String(DEVICE_UUID) + "/brightness")
-              .c_str());
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
+  if (client.connect(clientId.c_str())) {
+    client.subscribe(
+        ("com.rizalanggoro.selaras/" + String(DEVICE_UUID) + "/mode").c_str());
+    client.subscribe(
+        ("com.rizalanggoro.selaras/" + String(DEVICE_UUID) + "/brightness")
+            .c_str());
   }
+
+  return client.connected();
 }
+
+// void reconnect2() {
+//   while (!client.connected()) {
+//     if (client.connect(clientId.c_str())) {
+//       Serial.println("connected");
+//       // client.publish("rdaOutTopic", "hello world");
+
+//     } else {
+
+//       delay(5000);
+//     }
+//   }
+// }
 
 void setup() {
   Serial.begin(115200);
@@ -74,14 +87,26 @@ void setup() {
   Serial.println("\nWiFi connected!");
   Serial.printf("IP address: %s\n", WiFi.localIP().toString().c_str());
 
-  Serial.println("Connecting to MQTT broker...");
+  // Serve a simple webpage
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(200, "text/plain",
+                  "Hi! This is WebSerial demo. You can access the WebSerial "
+                  "interface at http://" +
+                      WiFi.localIP().toString() + "/webserial");
+  });
+
+  WebSerial.begin(&server);
+
+  server.begin();
+
+  WebSerial.println("Connecting to MQTT broker...");
   client.setServer(MQTT_BROKER, 1883);
   client.setCallback(callback);
 
   ws2812fx.init();
-  ws2812fx.setBrightness(100);
+  ws2812fx.setBrightness(1);
   ws2812fx.setSpeed(1000);
-  ws2812fx.setMode(FX_MODE_RAINBOW_CYCLE);
+  ws2812fx.setMode(FX_MODE_STATIC);
   ws2812fx.start();
 }
 
@@ -90,9 +115,24 @@ void setup() {
 // #define MSG_BUFFER_SIZE (50)
 // char msg[MSG_BUFFER_SIZE];
 
+unsigned long lastReconnectAttempt = 0;
+unsigned long lastStatsPrint = 0;
+
 void loop() {
-  if (!client.connected()) reconnect();
-  client.loop();
+  if (!client.connected()) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = currentMillis;
+      if (reconnect())
+        lastReconnectAttempt = 0;
+      else {
+        WebSerial.print("failed, rc=");
+        WebSerial.print(client.state());
+        WebSerial.println(" try again in 5 seconds");
+      }
+    }
+  } else
+    client.loop();
 
   // unsigned long now = millis();
   // if (now - lastMsg > 2000) {
@@ -105,4 +145,15 @@ void loop() {
   // }
 
   ws2812fx.service();
+
+  // Print every 2 seconds (non-blocking)
+  if ((unsigned long)(millis() - lastStatsPrint) > 2000) {
+    WebSerial.print(F("IP address: "));
+    WebSerial.println(WiFi.localIP());
+    WebSerial.printf("Uptime: %lums\n", millis());
+    WebSerial.printf("Free heap: %u\n", ESP.getFreeHeap());
+    lastStatsPrint = millis();
+  }
+
+  WebSerial.loop();
 }
